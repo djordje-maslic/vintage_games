58988ffad504698ab4871d8512685f0779bc4c55
maslic <djooordje@gmail.com>
2021-09-29 15:42:05 +0200

    comit1 for Hard | 1000 points | Topcoder Skill Builder Competition | Flutter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vintage_games/main.dart';
+import 'package:vintage_games/presentation/my_app.dart';
 
 void main() {
   testWidgets(
